c5fae503299a2c5a4ee0b5ea65ea828e5e2e6eb9
dcarsin@byui.edu <dcarsin@byui.edu>
2021-05-16 02:57:25 -0300

    entrega

--- WDD230/lesson4/js/script.js
+++ WDD230/lesson4/js/script.js
@@ -4,4 +4,11 @@ function toggleMenu(){
     document.getElementsByClassName("navigation")[0].classList.toggle('responsive');
 }
 
-document.write(new Date().getFullYear());
+
+const datefield = document.querySelector("data");
+const now = new Date();
+const fulldate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(now);
+
+document.querySelector('#year').innerHTML = `&copy;`+ new Date().getFullYear() + `- David Carsin - Uruguay - <a href="https://www.byui.edu/online" target="_blank">BYU-Idaho Online
+Learning</a>`;
+document.querySelector('#lastUpdate').innerHTML = "Last Updated: " + fulldate;
