4017a724a580efa165715becc37412d2dc3b4a1e
dcarsin <dcarsin@byui.edu>
2022-01-15 09:16:27 -0300

    test

---
+++ WDD330/js/main.js
@@ -0,0 +1,24 @@
+const links = [
+    {
+        label: "Week1 notes",
+        url: "index.html"
+    },
+    // {
+    //     label: "Week2 notes",
+    //     url: "algo/index.html"
+    // }
+]
+let i = 1;
+links.forEach(one => {
+    // let active = document.getElementsByTagName('ol');
+    // document.querySelector('.herodiv').innerHTML = "";
+    let list = document.createElement('li');
+    list.textContent = "Week " + i;
+    let att = document.createElement('a');
+    att.setAttribute('href', `${one.url}`);
+    att.textContent = ` ${one.label}`;
+    list.appendChild(att);
+    document.querySelector('.generateLines').appendChild(list);
+    // document.getElementById('herodiv').innerHTML = active;
+    i++;
+}, 100);
